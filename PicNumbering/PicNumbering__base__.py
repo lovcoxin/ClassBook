9988e750297893ee5796f881fddcf2d269015015
lovcoxin <131973516+lovcoxin@users.noreply.github.com>
2024-03-31 19:56:23 +0800

    Add files via upload

---
+++ PicNumbering/PicNumbering__base__.py
@@ -0,0 +1,42 @@
+"""
+因为做着太晕了，分个分区好处理
+分区模块def库：PicNumbering编码分区函数
+"""
+
+# --二级调用函数-------------------------------------
+# 根据dict参数打印key或value，
+def print_dict(nums, dict, param)
+    nums = int(nums)
+
+    dict_i = {
+        'key': keys,
+        'value': values
+    }
+
+    if param == 'key':
+        for i in dict.keys():
+            print('[' + nums + '] ' + i)
+
+    elif param == 'value':
+        for i in dict.values():
+            print('[' + nums + '] ' + i)
+
+
+#
+def
+
+
+
+
+# --一级调用函数：负责调用级别更低的细分函数-------------------------------------
+
+# create 创建
+def create_numbering():
+
+
+
+
+# retrieve 检索
+def retrieve_numbering():
+
+
