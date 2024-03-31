7bee32c6e769c42d556a7406f5cc1ea9b8799ffa
lxxgd <lxxgd@qq.com>
2024-03-31 22:04:25 +0800

    README.md

--- CBTool/obj/Debug/net8.0-windows/CBTool.AssemblyInfo.cs
+++ CBTool/obj/Debug/net8.0-windows/CBTool.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CBTool")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559bbb52a62904b94f8bbd1aa27add987bc72b8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CBTool")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CBTool")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
