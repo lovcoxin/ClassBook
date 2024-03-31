9c6a87fbb119d0a16e8fb0833278172e85b9d231
lovcoxin <131973516+lovcoxin@users.noreply.github.com>
2024-03-31 19:53:56 +0800

    Upload by PicGo

---
+++ CBtool.py
@@ -0,0 +1,24 @@
+from data_info import *
+from __base__ import *
+# from 子目录下PicNumbering base的函数
+
+# 自动粘贴板，copy(容器)
+from clipboard import copy
+
+'''
+
+Class book project tool with Python
+2024.03.30 21：56 尝试优化项目
+
+功能：
+给文件编码
+根据文件名进行预览.web
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+'''
+
+if __name__ == '__main__':
+    # list[标题，选项，选项..
+    # main talk defaultset
+    homelist = ['HomeGuide','about', 'PicNumbering', 'none']
+
+    call_in_talk(homelist)
